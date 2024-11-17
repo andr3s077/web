33b2afca258524c505f55386acd322c707e6f2eb
andr3s077 <jhonyberrezueta@gmail.com>
2024-11-16 21:55:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn2").addEventListener ("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+ScrollReveal().reveal('.titulo h1');
+ScrollReveal().reveal('.titulo2 h1');
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards', {delay: 400});
+ScrollReveal().reveal('.cards-banner-one', {delay: 400});
+ScrollReveal().reveal('.cards-banner-two', {delay: 400});
