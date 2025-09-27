a35c879421c8acb2e28277b4ff504debee30a653
andr3s077 <jhonyberrezueta@gmail.com>
2025-09-27 15:54:59 -0500

    codigo.py
    Código usado

---
+++ codigo.py
@@ -0,0 +1,11 @@
+import sys
+
+print("Máximo entero:", sys.maxsize)
+print("Mínimo entero:", -sys.maxsize - 1)
+
+print("Máximo flotante:", sys.float_info.max)
+print("Mínimo flotante positivo:", sys.float_info.min)
+print("Precisión de flotante:", sys.float_info.epsilon)
+
+
+Agrego el código que se uso para hallar la información requerida.
